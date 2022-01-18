b267f38bfa493304e08d6902b3c2ab3dc5e99bcb
gauthier poulain <poulain.gauthier16@gmail.com>
2022-01-18 21:27:00 +0100

    add docker-compose w/ postgres and format files

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,10 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { WsAdapter } from "@nestjs/platform-ws"
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { WsAdapter } from "@nestjs/platform-ws";
+const port = 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new WsAdapter(app));
-  await app.listen(3000);
+  await app.listen(port, () => {
+    console.info("Listening on http://localhost:3000");
+  });
 }
 bootstrap();
