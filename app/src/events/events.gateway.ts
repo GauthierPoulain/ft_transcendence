77b85682a4ed59184d218731754d8eccdbdf02bf
LightDiscord <root@arnaud.sh>
2022-01-18 20:41:49 +0100

    dummy websocket example

---
+++ app/src/events/events.gateway.ts
@@ -0,0 +1,13 @@
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets"
+import { Server } from "ws"
+
+@WebSocketGateway()
+export class EventsGateway {
+	@WebSocketServer()
+	server: Server
+
+	@SubscribeMessage("dummy")
+	async onEvent(@MessageBody() data: number): Promise<number> {
+		return data
+	}
+}
