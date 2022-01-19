539c3361561042e7497b2c763853b62f3c73d090
gauthier poulain <poulain.gauthier16@gmail.com>
2022-01-19 13:54:54 +0100

    sessions storage using db (manually disabled)

--- app/src/auth/auth.controller.ts
+++ app/src/auth/auth.controller.ts
@@ -1,26 +1,80 @@
-import { Controller, Get, Param, Query, Redirect, Res } from "@nestjs/common";
+import { Controller, Get, Query, Res, Session } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import axios, { AxiosResponse } from "axios";
+import session from "express-session";
 
 const api42Config = {
   uid: "fa0f13e6daf52c3590217ebadc4c2cd9e757fac12b997d6abf3ac7d089af7254",
-  redirect_uri: "http://localhost:3000/",
+  secret: "d92778332ac24fcfdafd152a871246f1b6c99e0fe28f555c3277106c60b9fdbb",
+  redirect_uri: "http://localhost:3000/auth/callback",
+  endpoint: "https://api.intra.42.fr/v2",
 };
+
+function api42request(
+  path: string,
+  token: string,
+): Promise<AxiosResponse<any, any>> {
+  return axios.get(api42Config.endpoint + path, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+}
 
 @Controller("auth")
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Get("redirect")
+  @Get()
   redirect(@Res() res) {
     return res.redirect(
-      `https://api.intra.42.fr/oauth/authorize?client_id=${api42Config.uid}&redirect_uri=${api42Config.redirect_uri}&response_type=code`,
+      `https://api.intra.42.fr/oauth/authorize?client_id=${
+        api42Config.uid
+      }&redirect_uri=${encodeURIComponent(
+        api42Config.redirect_uri,
+      )}&response_type=code`,
     );
   }
 
-  @Get("callback")
-  callback(@Query("code") code): string {
-    console.log(code);
+  @Get("debug")
+  debug(@Session() session: Record<string, any>): string {
+    return JSON.stringify(session);
+  }
 
-    return "ok";
+  @Get("callback")
+  async callback(
+    @Session() session: Record<string, any>,
+    @Query("code") code,
+    @Res() res,
+  ): Promise<string> {
+    if (!code) return "Error: missing code in get params";
+    try {
+      const data = (
+        await axios.post(
+          "https://api.intra.42.fr/oauth/token",
+          {
+            grant_type: "authorization_code",
+            client_id: api42Config.uid,
+            client_secret: api42Config.secret,
+            code: code,
+            redirect_uri: api42Config.redirect_uri,
+          },
+          { headers: { "content-type": "application/json" } },
+        )
+      ).data;
+      const userRaw = (await api42request("/me", data.access_token)).data;
+      const user = {
+        id: userRaw.id,
+        login: userRaw.login,
+        first_name: userRaw.first_name,
+        last_name: userRaw.last_name,
+        image_url: userRaw.image_url,
+      };
+      session.user = user;
+      return res.redirect("/auth/debug");
+    } catch (error) {
+      console.error(error);
+      return "Interal error";
+    }
   }
 }
--- app/src/main.ts
+++ app/src/main.ts
@@ -1,11 +1,47 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { WsAdapter } from "@nestjs/platform-ws";
+import * as session from "express-session";
+
 const port = 3000;
+const sessionStored = false;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useWebSocketAdapter(new WsAdapter(app));
+
+  const sessionSecret = "OwO";
+  if (sessionStored) {
+    const PostgreSqlStore = require("connect-pg-simple");
+    const PGCon = {
+      user: "postgres",
+      password: "postgres",
+      host: "localhost",
+      port: 5432,
+      database: "ft_transcendance",
+    };
+    app.use(
+      session({
+        secret: sessionSecret,
+        resave: true,
+        saveUninitialized: false,
+        store: new (PostgreSqlStore(session))({
+          conString: `pg://${PGCon.user}:${PGCon.password}@${PGCon.host}:${PGCon.port}/${PGCon.database}`,
+        }),
+        cookie: { maxAge: 2592000 }, // 30 days
+      }),
+    );
+  } else {
+    app.use(
+      session({
+        secret: sessionSecret,
+        resave: false,
+        saveUninitialized: false,
+      }),
+    );
+  }
+
   await app.listen(port, () => {
     console.info(`Listening on http://localhost:${port}`);
   });
