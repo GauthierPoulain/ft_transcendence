b41ff8e8cc0ab8ff96a349aee31f50d6bb178f64
LightDiscord <root@arnaud.sh>
2022-01-19 19:19:40 +0100

    oauth2 passport

--- app/src/auth/auth.controller.ts
+++ app/src/auth/auth.controller.ts
@@ -1,54 +1,47 @@
 import {
     Controller,
     Get,
-    Query,
-    Res,
     Session,
     Redirect,
-    BadRequestException,
+    Request,
+    UseGuards,
 } from "@nestjs/common";
-import { HttpService } from "@nestjs/axios";
-import { AxiosResponse } from "axios";
-import { AuthService } from "./auth.service";
-import { firstValueFrom } from "rxjs";
-import { Response } from "express";
 import { UsersService } from "../users/users.service"
-
-const client_id = process.env.API42UID;
-const client_secret = process.env.API42SECRET;
-const redirect_uri = "http://localhost:3000/auth/callback";
-const api_endpoint = "https://api.intra.42.fr/v2/";
-
-function api42request(
-    httpService: HttpService,
-    path: string,
-    token: string,
-): Promise<AxiosResponse<any, any>> {
-    return firstValueFrom(
-        httpService.get(new URL(path, api_endpoint).toString(), {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        }),
-    );
-}
-
-const oauth_url = new URL("/oauth/authorize", "https://api.intra.42.fr");
-oauth_url.searchParams.set("client_id", client_id);
-oauth_url.searchParams.set("redirect_uri", redirect_uri);
-oauth_url.searchParams.set("response_type", "code");
+import { OAuth2AuthGuard } from "./guards/oauth2-auth.guard";
 
 @Controller("auth")
 export class AuthController {
     constructor(
-        private authService: AuthService,
-        private httpService: HttpService,
         private usersService: UsersService,
     ) {}
 
+    @UseGuards(OAuth2AuthGuard)
     @Get()
-    @Redirect(oauth_url.toString())
-    redirect() {}
+    login() { }
+
+    @UseGuards(OAuth2AuthGuard)
+    @Get("callback")
+    @Redirect("/users/me")
+    async login_callback(
+        @Session() session: Record<string, any>,
+        @Request() req: any)
+    {
+        const { id, login, image_url } = req.user;
+
+        let user = await this.usersService.findIntra(id)
+
+        // In case no user with this intra exists, create it.
+        if (!user) {
+            user = await this.usersService.create({
+                intra_id: id,
+                intra_login: login,
+                intra_image_url: image_url
+            })
+        }
+
+        console.log(session)
+        session.user = user.id;
+    }
 
     @Get("debug")
     debug(@Session() session: Record<string, any>): string {
@@ -58,40 +51,6 @@ export class AuthController {
         return JSON.stringify(session);
     }
 
-    @Redirect("/users/me")
-    @Get("callback")
-    async callback(
-        @Session() session: Record<string, any>,
-        @Query("code") code: string,
-        @Res() res: Response,
-    ) {
-        if (!code) {
-            throw new BadRequestException();
-        }
-
-        const { data } = await firstValueFrom(
-            this.httpService.post("https://api.intra.42.fr/oauth/token", {
-                grant_type: "authorization_code",
-                client_id,
-                client_secret,
-                code,
-                redirect_uri,
-            }),
-        );
-
-        const {
-            data: { id, login, image_url },
-        } = await api42request(this.httpService, "me", data.access_token);
-
-        let user = await this.usersService.findIntra(id)
-
-        // In case no user with this intra exists, create it.
-        if (!user) {
-            user = await this.usersService.create({ intra_id: id, intra_login: login, intra_image_url: image_url })
-        }
-
-        session.user = user.id;
-    }
 
     @Get("logout")
     @Redirect("/auth/debug")
--- app/src/auth/auth.module.ts
+++ app/src/auth/auth.module.ts
@@ -5,10 +5,12 @@ import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
 
 import { UsersModule } from "../users/users.module";
+import { PassportModule } from "@nestjs/passport";
+import { OAuth2Strategy } from "./strategies/oauth.strategy";
 
 @Module({
-  imports: [HttpModule, UsersModule],
-  providers: [AuthService],
+  imports: [HttpModule, UsersModule, PassportModule],
+  providers: [AuthService, OAuth2Strategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
