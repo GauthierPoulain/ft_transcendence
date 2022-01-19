af3b096ab3beb2cebd6160ac327796e24db4b1b6
gauthier poulain <poulain.gauthier16@gmail.com>
2022-01-19 13:58:05 +0100

    use import

--- app/src/main.ts
+++ app/src/main.ts
@@ -2,9 +2,17 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { WsAdapter } from "@nestjs/platform-ws";
 import * as session from "express-session";
+import * as PGStore from "connect-pg-simple";
 
 const port = 3000;
 const sessionStored = false;
+const PGCon = {
+  user: "postgres",
+  password: "postgres",
+  host: "localhost",
+  port: 5432,
+  database: "ft_transcendance",
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,20 +21,12 @@ async function bootstrap() {
 
   const sessionSecret = "OwO";
   if (sessionStored) {
-    const PostgreSqlStore = require("connect-pg-simple");
-    const PGCon = {
-      user: "postgres",
-      password: "postgres",
-      host: "localhost",
-      port: 5432,
-      database: "ft_transcendance",
-    };
     app.use(
       session({
         secret: sessionSecret,
         resave: true,
         saveUninitialized: false,
-        store: new (PostgreSqlStore(session))({
+        store: new (PGStore(session))({
           conString: `pg://${PGCon.user}:${PGCon.password}@${PGCon.host}:${PGCon.port}/${PGCon.database}`,
         }),
         cookie: { maxAge: 2592000 }, // 30 days
