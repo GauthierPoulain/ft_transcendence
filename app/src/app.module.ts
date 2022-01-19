871378706419f1dac330718235d7531d9e59bbcc
LightDiscord <root@arnaud.sh>
2022-01-19 15:50:35 +0100

    typeorm database connection

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm"
+
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { EventsModule } from "./events/events.module";
@@ -6,8 +8,24 @@ import { UsersModule } from "./users/users.module";
 import { AuthModule } from "./auth/auth.module";
 
 @Module({
-  imports: [EventsModule, UsersModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot({
+            // TODO: Make them configurable.
+            type: "postgres",
+            host: "localhost",
+            port: 5432,
+            username: "postgres",
+            password: "postgres",
+            database: "ft_transcendance",
+            entities: [],
+
+            // TODO: This can destroy production data, so we may want to remove this in the future.
+            synchronize: true
+
+        }),
+        EventsModule, UsersModule, AuthModule
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
