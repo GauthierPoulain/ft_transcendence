1bf0691c207ffc27f1615dd69b5a88c736176697
LightDiscord <root@arnaud.sh>
2022-01-20 23:05:24 +0100

    frontend: router

---
+++ front/src/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <router-view />
+</template>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+
+h1 {
+    color: white;
+    text-align: center;
+    padding: 20px;
+    margin: 20px;
+    margin-left: 70vh;
+    margin-right: 70vh;
+    border-radius: 25px;
+    background: rgb(105, 105, 105);
+}
+
+.button {
+    text-align: center;
+}
+
+body {
+    background: rgb(68, 68, 68);
+}
+</style>
