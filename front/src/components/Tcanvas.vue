4104a268a0fe999150f27d2610a72c263ddc176b
neryss <neryssistos@gmail.com>
2022-01-20 17:09:13 +0100

    wip events, lots of errors with props

--- front/src/components/Tcanvas.vue
+++ front/src/components/Tcanvas.vue
@@ -1,7 +1,17 @@
-props: 'mWidth'
+<script setup lang="ts">
+import {ref} from 'vue';
+
+var x = ref(0);
+var y = ref(0);
+function showCoords(e) {
+    x.value = e.offsetX;
+    y.value = e.offsetY;
+}
+</script>
 
 <template>
-    <canvas id="myCanvas"/>
+    <span>{{x}}, {{y}}</span>
+    <canvas id="myCanvas" @mousemove="showCoords"/>
 </template>
 
 <style>
