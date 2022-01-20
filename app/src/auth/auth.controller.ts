096565cb4a2223f54cc6af8d6ed1153d15a6c4e5
Gauthier Poulain <gapoulai@student.42lyon.fr>
2022-01-20 13:49:14 +0100

    WORKING ON 2FA SORRY FOR CAPS LOCK

--- app/src/auth/auth.controller.ts
+++ app/src/auth/auth.controller.ts
@@ -5,21 +5,23 @@ import {
     Redirect,
     Request,
     UseGuards,
+    Query,
 } from "@nestjs/common";
 
-import { UsersService } from "../users/users.service"
+import { UsersService } from "../users/users.service";
 import { OAuth2AuthGuard } from "./guards/oauth2-auth.guard";
 
-import { promisify } from "util"
+import * as twoFactors from "node-2fa";
+
+import { promisify } from "util";
 
 @Controller("auth")
 export class AuthController {
-    constructor(private usersService: UsersService) {
-    }
+    constructor(private usersService: UsersService) {}
 
     @UseGuards(OAuth2AuthGuard)
     @Get()
-    login() { }
+    login() {}
 
     @UseGuards(OAuth2AuthGuard)
     @Get("callback")
@@ -38,21 +40,48 @@ export class AuthController {
             });
         }
 
-        await promisify(req.logIn.bind(req))(user)
+        await promisify(req.logIn.bind(req))(user);
     }
 
     @Get("debug")
-    debug(@Session() session: Record<string, any>, @Request() req: any): string {
-        console.log("debug");
-        console.log(session);
-
+    debug(
+        @Session() session: Record<string, any>,
+        @Request() req: any,
+    ): string {
         return `session id : ${req.sessionID}<br>${JSON.stringify(session)}`;
     }
 
     @Get("logout")
     @Redirect("/auth/debug")
     async logout(@Request() req: any) {
-        req.logOut()
-        await promisify(req.session.destroy.bind(req.session))()
+        req.logOut();
+        await promisify(req.session.destroy.bind(req.session))();
+    }
+
+    @Get("2fa")
+    tfa() {
+        const appName = "ft_transcendance";
+        const userName = "bite";
+
+        let secret = twoFactors.generateSecret({
+            name: appName,
+            account: userName,
+        });
+
+        console.log(secret);
+
+        return secret.qr;
+    }
+
+    @Get("a")
+    a() {
+        return twoFactors.generateToken("W67SXM5AAQBLYUDYYWGOBWYHLUD2KYTD");
+    }
+
+    @Get("b")
+    b(@Query("code") code) {
+        return JSON.stringify(
+            twoFactors.verifyToken("W67SXM5AAQBLYUDYYWGOBWYHLUD2KYTD", code),
+        );
     }
 }
--- app/src/users/users.controller.ts
+++ app/src/users/users.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get, Request, UseGuards } from "@nestjs/common"
-import { User } from "./entities/user.entity"
-import { ConnectedGuard } from "src/auth/guards/connected.guard"
+import { Controller, Get, Request, UseGuards } from "@nestjs/common";
+import { User } from "./entities/user.entity";
+import { ConnectedGuard } from "src/auth/guards/connected.guard";
 
 @Controller("users")
 export class UsersController {
     @UseGuards(ConnectedGuard)
-	@Get("me")
-	me(@Request() request: any): Promise<User> {
-		return request.user
-	}
+    @Get("me")
+    me(@Request() request: any): Promise<User> {
+        return request.user;
+    }
 }
