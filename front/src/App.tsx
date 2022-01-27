4c8621d0b1c626167a387371b21fb88c87c2dc5a
neryss <neryssistos@gmail.com>
2022-01-27 23:04:04 +0100

    wip displayable thingies

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import Tcanvas from "./components/Tcanvas";
 import Cbutton from "./components/Cbutton";
 
--- front/src/components/Cbutton.tsx
+++ front/src/components/Cbutton.tsx
@@ -1,10 +1,30 @@
 import React from "react";
 import "../static/styles/button.css"
 
+var done = false;
+
+function test() {
+    console.log("hop clicked");
+    if (done)
+    {
+        var toDel = document.getElementById("sub");
+        toDel?.remove();
+        done = false;
+        return ;
+    }
+    var tag = document.createElement("p");
+    tag.setAttribute("id", "sub");
+    var text = document.createTextNode("C'est génial ici!");
+    tag.appendChild(text);
+    var element = document.getElementById('test');
+    element?.appendChild(tag);
+    done = true;
+}
+
 function Cbutton(props: any) {
     return (
         <React.Fragment>
-            <button>{props.children}</button>
+            <button id="test" onClick={test}>{props.children}</button>
         </React.Fragment>
     );
 }
