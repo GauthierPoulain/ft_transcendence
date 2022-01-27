1773ec43767baa11f02b7d808d915bd44feedd3b
neryss <neryssistos@gmail.com>
2022-01-28 00:09:06 +0100

    wip on click element

--- front/src/components/Cbutton.tsx
+++ front/src/components/Cbutton.tsx
@@ -3,20 +3,33 @@ import "../static/styles/button.css"
 
 var done = false;
 
-function test() {
-    console.log("hop clicked");
+function createElem(event:any) {
+	var tag = document.createElement("p");
+    tag.setAttribute("id", "sub");
+    tag.style.position = "absolute";
+    tag.style.top = event.pageY + "px";
+    tag.style.left = event.pageX + "px";
+    tag.style.transform = "translate(-50%, -50%)";
+	tag.style.color = "red";
+    console.log(tag.style.top);
+    console.log(event.pageX);
+    tag.style.position = 'absolute';
+	var text = document.createTextNode("C'est génial ici!");
+	tag.appendChild(text);
+	return (tag)
+}
+
+function test(event:any) {
+	console.log("hop clicked");
     if (done)
     {
-        var toDel = document.getElementById("sub");
+		var toDel = document.getElementById("sub");
         toDel?.remove();
         done = false;
         return ;
     }
-    var tag = document.createElement("p");
-    tag.setAttribute("id", "sub");
-    var text = document.createTextNode("C'est génial ici!");
-    tag.appendChild(text);
-    var element = document.getElementById('test');
+	var tag = createElem(event);
+    var element = document.getElementById('root');
     element?.appendChild(tag);
     done = true;
 }
