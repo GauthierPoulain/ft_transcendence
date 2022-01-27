d7ca445975aa3c4bc4faa86d0bcd730343668b96
Gauthier Poulain <gapoulai@student.42lyon.fr>
2022-01-27 16:40:42 +0100

    tkt

--- front/src/Tcanvas.tsx
+++ front/src/Tcanvas.tsx
@@ -3,7 +3,6 @@ import React, { Fragment } from 'react';
 function Tcanvas(props:any) {
 	return (
 		<Fragment>
-			<script src='/pong.js'></script>
 			<script>pong({props.width}, {props.height})</script>
 			<canvas id="pong" style={{width: props.width, height: props.height}}></canvas>
 		</Fragment>
