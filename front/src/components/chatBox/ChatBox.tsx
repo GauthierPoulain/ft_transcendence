978f9ff8fc99c41f2c4d665bc02d02f88139e8db
ldevillard <logandvllrd@gmail.com>
2022-02-03 11:30:22 +0100

    First front chat set

--- front/src/components/chatBox/ChatBox.tsx
+++ front/src/components/chatBox/ChatBox.tsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom'
 import { Home } from '@material-ui/icons'
 import { Person } from '@material-ui/icons'
 
-
 function ChatBox(props:any) {
     return (
         <div className="chatContainer">
@@ -15,7 +14,9 @@ function ChatBox(props:any) {
 
             <div className="chatcenter">
                 <div className="chat">
-                    <span className="chatname">ldevilla's chat</span>
+                    <span className="chatname">#student's chat</span>
+                    <div className="chatbox"></div>
+                    <input type="text" placeholder="type something..." className="inputHolder"/>
                 </div>
             </div>
 
