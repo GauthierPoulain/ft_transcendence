f3f4fd9891345c5c24144f100d57443c9c99d15b
ldevillard <logandvllrd@gmail.com>
2022-02-03 12:32:24 +0100

    Continue test scripting chat window

--- front/src/components/chatBox/ChatBox.tsx
+++ front/src/components/chatBox/ChatBox.tsx
@@ -5,13 +5,32 @@ class ChatBox extends React.Component {
 
     state = 
     {
-        nb: 0
+        nb: 0,//TEST
+        chat: [
+            {str: "hey"},
+            {str: "pouic"},
+            {str: "lul"}
+        ],
+        chan:
+        [
+            {name: "#ldevilla"},
+            {name: '#ckurt'},
+            {name: '#gapoulai'},
+            {name: '#arpascal'}
+        ]
     };
 
-    add()
+    add()//TEST
     {
         let nbr = this.state.nb++;
         this.setState({nbr});
+    }
+
+    addChan()
+    {
+        let chanList = this.state.chan;
+        chanList.push({name: "#newChan"});
+        this.setState(chanList);
     }
 
     render()
@@ -20,7 +39,18 @@ class ChatBox extends React.Component {
         <div className="chatContainer">
             <div className="chatleft">
                 <div className="channels">
-                    <span className="chattitle">Channels</span>
+                    <span className="chattitle">Discussion</span><br /><br />
+                    <div className="chan">
+                        <span>channels</span>
+                        <button id="addChan" onClick={() => this.addChan()}>+</button>
+                    </div>
+                    <div className="chanList">
+                        {this.state.chan.map(msg => (
+                            <div>
+                                <span>{msg.name}</span> <br />
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
 
@@ -30,6 +60,11 @@ class ChatBox extends React.Component {
                     <div className="chatbox">
                         <button onClick={() => this.add()}>+</button>
                         <span>{this.state.nb}</span>
+                        {this.state.chat.map(msg => (
+                            <div>
+                                <span>{msg.str}</span> <br />
+                            </div>
+                        ))}
                     </div>
                     <input type="text" placeholder="type something..." className="inputHolder"/>
                 </div>
