b24dcbf8f8028c7c39a22944833ea7146d54383d
ldevillard <logandvllrd@gmail.com>
2022-02-04 09:30:03 +0100

    Start working with bootstrap, wtf i just put 3min and i made leaderboard page instead of one day for chat page

--- front/src/components/chatBox/ChatBox.tsx
+++ front/src/components/chatBox/ChatBox.tsx
@@ -1,106 +1,103 @@
-import "./chat.css"
+import "./chat.css";
 import React, { Component } from "react";
 
 class ChatBox extends React.Component {
-
-    state = 
-    {
-        nb: 0,//TEST
-        chat: [
-            {str: "Welcome to chat room"},
+    state = {
+        nb: 0, //TEST
+        chat: [{ str: "Welcome to chat room" }],
+        buf: "",
+        chan: [
+            { name: "#ldevilla" },
+            { name: "#ckurt" },
+            { name: "#gapoulai" },
+            { name: "#arpascal" },
         ],
-        buf: ""
-        ,
-        chan:
-        [
-            {name: "#ldevilla"},
-            {name: '#ckurt'},
-            {name: '#gapoulai'},
-            {name: '#arpascal'}
-        ]
     };
 
-    add()//TEST
-    {
+    add() { //TEST
         let nbr = this.state.nb++;
-        this.setState({nbr});
+        this.setState({ nbr });
     }
 
-    addChan()
-    {
+    addChan() {
         let chanList = this.state.chan;
-        chanList.push({name: "#newChan"});
+        chanList.push({ name: "#newChan" });
         this.setState(chanList);
     }
 
-    msgSubmit = event =>
-    {
-
+    msgSubmit = (event) => {
         event.preventDefault();
 
         const msg = this.state.buf;
         const chat = this.state.chat;
 
-        if (this.state.buf !== "")
-        {
+        if (this.state.buf !== "") {
             console.log(msg);
-            chat.push({str: msg});
+            chat.push({ str: msg });
             this.setState(chat);
-            this.setState({buf: ""});
+            this.setState({ buf: "" });
         }
-    }
+    };
 
-    msgChange = event =>
-    {
-        this.setState({ buf: event.currentTarget.value});
-    }
+    msgChange = (event) => {
+        this.setState({ buf: event.currentTarget.value });
+    };
 
-    render()
-    {
-    return (
-        <div className="chatContainer">
-            <div className="chatleft">
-                <div className="channels">
-                    <span className="chattitle">Discussion</span><br /><br />
-                    <div className="chan">
-                        <span>channels</span>
-                        <button id="addChan" onClick={() => this.addChan()}>+</button>
+    render() {
+        return (
+            <div className="chatContainer">
+                <div className="chatleft">
+                    <div className="channels">
+                        <span className="chattitle">Discussion</span>
+                        <br />
+                        <br />
+                        <div className="chan">
+                            <span>channels</span>
+                            <button id="addChan" onClick={() => this.addChan()}>
+                                +
+                            </button>
+                        </div>
+                        <div className="chanList">
+                            {this.state.chan.map((msg) => (
+                                <div>
+                                    <span>{msg.name}</span> <br />
+                                </div>
+                            ))}
+                        </div>
                     </div>
-                    <div className="chanList">
-                        {this.state.chan.map(msg => (
-                            <div>
-                                <span>{msg.name}</span> <br />
-                            </div>
-                        ))}
+                </div>
+
+                <div className="chatcenter">
+                    <div className="chat">
+                        <span className="chatname">#student's chat</span>
+                        <div className="chatbox">
+                            <button onClick={() => this.add()}>+</button>
+                            <span>{this.state.nb}</span>
+                            {this.state.chat.map((msg) => (
+                                <div>
+                                    <span>{msg.str}</span> <br />
+                                </div>
+                            ))}
+                        </div>
+                        <form onSubmit={this.msgSubmit}>
+                            <input
+                                type="text"
+                                value={this.state.buf}
+                                onChange={this.msgChange}
+                                placeholder="type something..."
+                                className="inputHolder"
+                            />
+                        </form>
+                    </div>
+                </div>
+
+                <div className="chatright">
+                    <div className="friendschat">
+                        <span className="chattitle">Friends</span>
                     </div>
                 </div>
             </div>
-
-            <div className="chatcenter">
-                <div className="chat">
-                    <span className="chatname">#student's chat</span>
-                    <div className="chatbox">
-                        <button onClick={() => this.add()}>+</button>
-                        <span>{this.state.nb}</span>
-                        {this.state.chat.map(msg => (
-                            <div>
-                                <span>{msg.str}</span> <br />
-                            </div>
-                        ))}
-                    </div>
-                    <form onSubmit={this.msgSubmit}>
-                        <input type="text" value={this.state.buf} onChange={this.msgChange} placeholder="type something..." className="inputHolder"/>
-                    </form>
-                </div>
-            </div>
-
-            <div className="chatright">
-                <div className="friendschat">
-                    <span className="chattitle">Friends</span>
-                </div>
-            </div>
-        </div>
-    )
+        );
     }
 }
 
--- front/src/pages/leaderboard/Leaderboard.tsx
+++ front/src/pages/leaderboard/Leaderboard.tsx
@@ -1,13 +1,42 @@
-import Topbar from "../../components/topbar/Topbar"
-import "./leaderboard.css"
+import Topbar from "../../components/topbar/Topbar";
+import "./leaderboard.css";
+import { Table } from "react-bootstrap";
 
 function Leaderboard() {
     return (
-		<div>
-			<Topbar />
-            <h1>Leaderboard</h1>
-		</div>
-    )
+        <div>
+            <Topbar />
+            <Table striped bordered hover variant="dark">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Username</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td colSpan={2}>Larry the Bird</td>
+                        <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </Table>
+        </div>
+    );
 }
 
 export default Leaderboard;
