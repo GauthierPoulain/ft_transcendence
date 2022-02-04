7853308118ee7b1ee5f4d885b3a951e89b8c21d7
ldevillard <logandvllrd@gmail.com>
2022-02-04 15:27:44 +0100

    Leaderboard front and script done

---
+++ front/src/components/LeaderboardTab/LeaderboardTab.tsx
@@ -0,0 +1,53 @@
+import "./style.css";
+import React, { Component } from "react";
+import { Table } from "react-bootstrap";
+
+class LeaderboardTab extends React.Component {
+
+    state =
+    {
+        players:[
+            {rank:"1", login:"arpascal", victories:"32"},
+            {rank:"2", login:"ckurt", victories:"29"},
+            {rank:"3", login:"gapoulai", victories:"12"},
+            {rank:"4", login:"ldevilla", victories:"4"}
+            //DB
+        ]
+    };
+
+    add()
+    {
+        let player = this.state.players;
+        player.push({rank:"?", login:"newPlayer", victories:"?"})
+        this.setState(player);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1 className="leaderboardTitle">Leaderboard</h1>
+                <Table striped bordered hover variant="dark">
+                    <thead>
+                        <tr>
+                            <th>#rank</th>
+                            <th>Login</th>
+                            <th>Victories</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.players.map((player) =>(
+                            <tr>
+                                <td>{player.rank}</td>
+                                <td>{player.login}</td>
+                                <td>{player.victories}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+                <button onClick={() => this.add()}>+</button>
+            </div>
+        );
+    }
+}
+
+export default LeaderboardTab;
