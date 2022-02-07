bc24caaf9ed2b4e302cd43de767d7d74089d9b29
ldevillard <logandvllrd@gmail.com>
2022-02-07 17:44:06 +0100

    matches front

--- front/src/components/profileban/Matches.tsx
+++ front/src/components/profileban/Matches.tsx
@@ -1,11 +1,30 @@
-import "./matches.css"
+import "./matches.css";
+import { Table } from "react-bootstrap";
 
 function Matches() {
     return (
         <div className="matchesContainer">
-            <h1>MATCHES</h1>
+            <h1 className="title">MATCHES</h1>
+            <Table striped bordered hover variant="dark">
+                <thead>
+                    <tr>
+                        <th>Against</th>
+                        <th>Winner</th>
+                        <th>Opponent score</th>
+                        <th>Your score</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>gapoulai</td>
+                        <td>ldevilla</td>
+                        <td>4</td>
+                        <td>5</td>
+                    </tr>
+                </tbody>
+            </Table>
         </div>
-    )
+    );
 }
 
 export default Matches;
