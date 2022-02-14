06485ecb4ee3cb4d3b208edbbac1650f31b0640e
ldevillard <logandvllrd@gmail.com>
2022-02-14 16:52:28 +0100

    add style for chanlist join

--- front/src/components/chatBox/ChatJoin.tsx
+++ front/src/components/chatBox/ChatJoin.tsx
@@ -1,14 +1,15 @@
-import "./chatjoin.css"
+import "./chatjoin.scss"
 import useChannel, { useJoinedChannels, usePublicChannels } from "../../data/use-channel"
+import { Button } from "react-bootstrap"
 
 function JoinPublic({ channelId }) {
-    return <button>Join</button>
+    return <Button variant="primary" className="chanButton">Join</Button>
 }
 
 function JoinProtected({ channelId }) {
     return <form>
-        <input type="text" required placeholder="Enter password..."></input>
-        <button type="submit">Join</button>
+        <input type="text" className="chanPlaceholder" required placeholder="Enter password..."></input>
+        <Button type="submit" variant="warning" className="chanButton">Join</Button>
     </form>
 }
 
@@ -16,8 +17,8 @@ function ChannelJoinCard({ channelId }) {
     const channel = useChannel(channelId)
 
     return (
-        <div>
-            <h3>{ channel.name }</h3>
+        <div className="channelsList">
+            <h3 className="name">{ channel.name }</h3>
 
             { channel.type === "public" && <JoinPublic channelId={channelId} /> }
             { channel.type === "protected" && <JoinProtected channelId={channelId} /> }
@@ -35,7 +36,9 @@ export default function ChatBox() {
         <div>
             <h2 className="chatJoinContainer">Join or create a wanted channel !</h2>
 
-            { channels.map((channelId) => <ChannelJoinCard key={channelId} channelId={channelId} />) }
+            <div className="chanListContainer">
+                { channels.map((channelId) => <ChannelJoinCard key={channelId} channelId={channelId} />) }
+            </div>
         </div>
     )
 }
