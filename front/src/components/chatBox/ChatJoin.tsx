a0e9c8fb1f7b17e63ce8f3db98dd8d1987579ede
LightDiscord <root@arnaud.sh>
2022-02-14 15:48:59 +0100

    joinable form

--- front/src/components/chatBox/ChatJoin.tsx
+++ front/src/components/chatBox/ChatJoin.tsx
@@ -1,5 +1,16 @@
 import "./chatjoin.css"
-import useChannel, { usePublicChannels } from "../../data/use-channel"
+import useChannel, { useJoinedChannels, usePublicChannels } from "../../data/use-channel"
+
+function JoinPublic({ channelId }) {
+    return <button>Join</button>
+}
+
+function JoinProtected({ channelId }) {
+    return <form>
+        <input type="text" required placeholder="Enter password..."></input>
+        <button type="submit">Join</button>
+    </form>
+}
 
 function ChannelJoinCard({ channelId }) {
     const channel = useChannel(channelId)
@@ -7,16 +18,23 @@ function ChannelJoinCard({ channelId }) {
     return (
         <div>
             <h3>{ channel.name }</h3>
+
+            { channel.type === "public" && <JoinPublic channelId={channelId} /> }
+            { channel.type === "protected" && <JoinProtected channelId={channelId} /> }
         </div>
     )
 }
 
 export default function ChatBox() {
-    const channels = usePublicChannels()
+    const pubs = usePublicChannels()
+    const joined = useJoinedChannels()
+
+    const channels = pubs.filter((id) => !joined.includes(id))
 
     return (
         <div>
             <h2 className="chatJoinContainer">Join or create a wanted channel !</h2>
+
             { channels.map((channelId) => <ChannelJoinCard key={channelId} channelId={channelId} />) }
         </div>
     )
