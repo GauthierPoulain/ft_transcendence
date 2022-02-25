20c613b5b8ec8968d72b2fb08659851ddf5e82d5
ldevillard <logandvllrd@gmail.com>
2022-02-25 14:02:28 +0100

    Adding curent matches table

--- front/src/pages/home/Home.tsx
+++ front/src/pages/home/Home.tsx
@@ -3,7 +3,64 @@ import Pong from "../../components/Pong/Pong"
 import React from "react"
 import { Link } from "react-router-dom"
 import "./home.scss"
-import { Button } from "react-bootstrap"
+import { Button, Table, OverlayTrigger, Tooltip } from "react-bootstrap"
+import { Visibility } from "@material-ui/icons"
+
+function RunningMatches()
+{
+    return (
+		<>
+		<h1 className="mb-2 ms-2 game-title">Matches in progress</h1>
+		<Table striped bordered hover variant="dark">
+            <thead>
+                <tr>
+                    <th>Player1</th>
+                    <th>Player2</th>
+                    <th></th>
+                </tr>
+            </thead>
+			<tbody>
+				<tr>
+					<td>Pouic</td>
+					<td>Pouet</td>
+					<td>
+						<OverlayTrigger placement="right" overlay={<Tooltip>View Match</Tooltip>}>
+							<Visibility className="game-view"/>
+						</OverlayTrigger>
+					</td>
+				</tr>
+				<tr>
+					<td>Pouic</td>
+					<td>Pouet</td>
+					<td>
+						<OverlayTrigger placement="right" overlay={<Tooltip>View Match</Tooltip>}>
+							<Visibility className="game-view"/>
+						</OverlayTrigger>
+					</td>
+				</tr>
+				<tr>
+					<td>Pouic</td>
+					<td>Pouet</td>
+					<td>
+						<OverlayTrigger placement="right" overlay={<Tooltip>View Match</Tooltip>}>
+							<Visibility className="game-view"/>
+						</OverlayTrigger>
+					</td>
+				</tr>
+				<tr>
+					<td>Pouic</td>
+					<td>Pouet</td>
+					<td>
+						<OverlayTrigger placement="right" overlay={<Tooltip>View Match</Tooltip>}>
+							<Visibility className="game-view"/>
+						</OverlayTrigger>
+					</td>
+				</tr>
+			</tbody>
+        </Table>
+		</>
+    )
+}
 
 function Home() {
     return (
@@ -22,6 +79,9 @@ function Home() {
                     </Link>
                 </Button>
             </div>
+			<div className="mt-5">
+            	<RunningMatches />
+			</div>
         </React.Fragment>
     )
 }
