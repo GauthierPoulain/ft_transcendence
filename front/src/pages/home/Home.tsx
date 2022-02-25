042d5ea281a560e6859da270be26ecb745b0be56
ldevillard <logandvllrd@gmail.com>
2022-02-25 13:39:39 +0100

    start work home page

--- front/src/pages/home/Home.tsx
+++ front/src/pages/home/Home.tsx
@@ -2,14 +2,26 @@ import Topbar from "../../components/topbar/Topbar"
 import Pong from "../../components/Pong/Pong"
 import React from "react"
 import { Link } from "react-router-dom"
+import "./home.scss"
+import { Button } from "react-bootstrap"
 
 function Home() {
     return (
         <React.Fragment>
-            <h1>uwu</h1>
-            <Link className="nav-link" to="/game">
-                pour les g@m3rz
-            </Link>
+            <div className="container-fluid welcome-bar">
+                <div className="text-center my-5">
+                    <h1>Welcome to ft_pong</h1>
+                    <p className="desc">Let's play a pong match !</p>
+                    <img src="/assets/pong.png" className="w-25" alt="" />
+                </div>
+            </div>
+            <div className="d-flex justify-content-center">
+                <Button className="play-button p-2" size="lg" variant="warning">
+                    <Link className="text" to="/game">
+                        PLAY
+                    </Link>
+                </Button>
+            </div>
         </React.Fragment>
     )
 }
