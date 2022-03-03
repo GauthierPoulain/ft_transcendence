727a66af18c6a8dd862d95a4c7338d2b71ac056c
ldevillard <logandvllrd@gmail.com>
2022-03-03 23:32:08 +0100

    Friends work on user page

--- front/src/pages/users/friends/index.tsx
+++ front/src/pages/users/friends/index.tsx
@@ -2,48 +2,50 @@ import { Brightness1 } from "@mui/icons-material"
 import { Image, Container } from "react-bootstrap"
 import { Link } from "react-router-dom"
 import "./friends.scss"
+import { useAuth } from "../../../data/use-auth"
+import { useUser } from "../../../data/users"
+import { useRelations } from "../../../data/relations"
+import { useStatus } from "../../../data/status"
+import { statusText } from "../../../data/status"
+import { statusColor } from "../../../data/status"
 
-function Friend({ pic, user, status }) {
+function Friend({ userId }) {
+    const user = useUser(userId)
+    const url = "/users/" + user.id
+    const status = useStatus(user.id)
+
     return (
-        <div className="d-flex border border-dark border-2 m-2 p-2 friends-card">
-            <Image
-                src={pic}
-                className="img"
-                style={{ height: "4em", width: "4em" }}
-            />
-            <Link to="#" className="m-auto mx-3 fs-4 text-decoration-none">
-                {user}
+        <div className="d-flex friends-card border border-dark border-2 m-2 p-2">
+            <Image className="img" src={user.image} style={{ height: "4em", width: "4em" }} />
+            <Link to={url} className="m-auto mx-3 fs-4 text-decoration-none">
+                {user.nickname}
             </Link>
             <div className="m-auto d-flex">
                 <span className="text-white text-uppercase m-auto">
-                    {status}
+                    {statusText(status)}
                 </span>
-                <Brightness1
-                    className="mx-2 mb-1"
-                    style={{
-                        color:
-                            status === "ONLINE"
-                                ? "lime"
-                                : status === "OFFLINE"
-                                ? "red"
-                                : "yellow",
-                    }}
-                />
+                <Brightness1 className="mx-2 mb-1" style={{ color: statusColor(status) }} />
             </div>
         </div>
     )
 }
 
 export default function Friends() {
-    return (
-        <Container>
-            <h2>Friends</h2>
+    const auth = useAuth()
+    const relations = useRelations()
+    const friends = relations.filter(
+        ({ currentId, kind }) => currentId === auth.userId && kind === "friend"
+    )
 
-            <div className="d-flex flex-wrap">
-                <Friend pic="/assets/42.jpg" user="ldevilla" status="ONLINE" />
-                <Friend pic="/assets/42.jpg" user="pouet" status="OFFLINE" />
-                <Friend pic="/assets/42.jpg" user="pouic" status="IN GAME" />
-            </div>
-        </Container>
+    if (friends.length === 0) {
+        return <p>You don't have any friend!</p>
+    }
+
+    return (
+        <div className="d-flex container">
+            {friends.map(({ targetId }) => (
+                <Friend key={targetId} userId={targetId} />
+            ))}
+        </div>
     )
 }
