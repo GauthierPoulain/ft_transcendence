c96dd96bc05d5b2c08456db8d89945453046a0d6
Logan Devillard <logandvllrd@gmail.com>
2022-03-10 15:07:27 +0100

    try catch UsernameSection

--- front/src/pages/users/settings/UsernameSection.tsx
+++ front/src/pages/users/settings/UsernameSection.tsx
@@ -1,14 +1,20 @@
-import { useState } from "react";
-import { Button, Form } from "react-bootstrap";
-import { useAuth } from "../../../data/use-auth";
-import { fetcher, useSubmit } from "../../../data/use-fetch";
-import { useUser } from "../../../data/users";
+import { useState } from "react"
+import { Button, Form } from "react-bootstrap"
+import { useAuth } from "../../../data/use-auth"
+import { fetcher, useSubmit } from "../../../data/use-fetch"
+import { useUser } from "../../../data/users"
 
 function useMutateName() {
-    return useSubmit<string, void>((name) => fetcher(`/settings/name`, {
-        method: "POST",
-        body: JSON.stringify({ name })
-    }, false))
+    return useSubmit<string, void>((name) =>
+        fetcher(
+            `/settings/name`,
+            {
+                method: "POST",
+                body: JSON.stringify({ name }),
+            },
+            false
+        )
+    )
 }
 
 export default function UsernameSection() {
@@ -18,21 +24,38 @@ export default function UsernameSection() {
 
     const { submit, isLoading } = useMutateName()
 
-    function submitForm(event: any) {
+    async function submitForm(event: any) {
         event.preventDefault()
-        return submit(name)
+        try {
+            await submit(name)
+        } catch (e) {
+            console.log(e)
+        }
     }
 
     return (
         <Form className="mb-3" onSubmit={submitForm}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Your custom username</Form.Label>
-                <Form.Control type="text" className="border-dark bg-dark" placeholder="Enter custom username" value={name} onChange={(event) => setName(event.target.value)}/>
-                <Form.Text className="text-muted">Use an empty username to remove it.</Form.Text>
+                <Form.Control
+                    type="text"
+                    className="border-dark bg-dark"
+                    placeholder="Enter custom username"
+                    value={name}
+                    onChange={(event) => setName(event.target.value)}
+                />
+                <Form.Text className="text-muted">
+                    Use an empty username to remove it.
+                </Form.Text>
             </Form.Group>
 
-            <Button size="sm" variant="primary" type="submit" disabled={isLoading}>
-                { name.length === 0 ? "Remove" : "Change" } custom username
+            <Button
+                size="sm"
+                variant="primary"
+                type="submit"
+                disabled={isLoading}
+            >
+                {name.length === 0 ? "Remove" : "Change"} custom username
             </Button>
         </Form>
     )
