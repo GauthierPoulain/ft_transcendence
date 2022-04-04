668499a9caa420f22f318a807e3937fd7d679534
LightDiscord <root@arnaud.sh>
2022-04-04 19:23:22 +0200

    404 todo game view

--- front/src/components/error/ErrorBox.tsx
+++ front/src/components/error/ErrorBox.tsx
@@ -1,9 +1,10 @@
+import { Link } from "react-router-dom"
 import { HttpError } from "../../errors/HttpError"
 
 function title(error: any): string {
     if (error instanceof HttpError) {
         if (error.status === 404) {
-            return "The resource doesn't exist."
+            return "The resource doesn't exist"
         }
 
         if (error.status === 401) {
@@ -14,10 +15,14 @@ function title(error: any): string {
     return "Too bad, an error occured..."
 }
 
-function subtitle(error: any): string {
+function subtitle(error: any) {
     if (error instanceof HttpError) {
         if (error.status === 401) {
             return "Go see somewhere else!"
+        }
+
+        if (error.status === 404) {
+            return <Link to="/" className="text-decoration-none text-white">Go back to home page</Link>
         }
     }
 
@@ -26,7 +31,7 @@ function subtitle(error: any): string {
 
 export function ErrorBox({ error }) {
     return (
-        <div className="flex-grow-1 d-flex justify-content-center align-items-center">
+        <div className="flex-grow-1 d-flex justify-content-center align-items-center text-center">
             <div>
                 <h2>{title(error)}</h2>
                 <p>{subtitle(error)}</p>
--- front/src/pages/game/view.tsx
+++ front/src/pages/game/view.tsx
@@ -1,13 +1,18 @@
 import { useParams } from "react-router-dom"
+import { ErrorBox } from "../../components/error/ErrorBox"
 import Pong from "../../components/Pong/Pong"
 import { useMatch } from "../../data/matches"
+import { HttpError } from "../../errors/HttpError"
 import GameViewWaiting from "./view/waiting"
 
-// TODO: 404 if useMatch is null
 export default function GameView() {
     const params = useParams()
     const gameId = parseInt(params.gameId as string, 10)
-    const match = useMatch(gameId)!
+    const match = useMatch(gameId)
+
+    if (!match) {
+        return <ErrorBox error={new HttpError(404)} />
+    }
 
     if (match.state === "waiting") {
         return <GameViewWaiting matchId={match.id} />
