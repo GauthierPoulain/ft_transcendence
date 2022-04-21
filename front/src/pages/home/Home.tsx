23f456791d5145d972774258bf58c9ba5b5eeaa5
Gauthier Poulain <poulain.gauthier16@gmail.com>
2022-04-21 15:09:00 +0200

    stylé

--- front/src/pages/home/Home.tsx
+++ front/src/pages/home/Home.tsx
@@ -4,13 +4,90 @@ import { Button, Container } from "react-bootstrap"
 import "./home.scss"
 import { useAuth } from "../../data/use-auth"
 import SectionFriends from "./friends"
+import Particles from "react-tsparticles"
+import { loadFull } from "tsparticles"
 import SectionMatches from "./matches"
+import { Engine } from "tsparticles-engine"
+
+function Animation(props: any) {
+    const particlesInit = async (main: Engine) => {
+        await loadFull(main)
+    }
+
+    return (
+        <Particles
+            id="tsparticles"
+            init={particlesInit}
+            options={{
+                background: {
+                    color: {
+                        value: "",
+                    },
+                },
+                fpsLimit: 144,
+                interactivity: {
+                    detect_on: "window",
+                    events: {
+                        resize: true,
+                    },
+                },
+                fullScreen: {
+                    enable: true,
+                    zIndex: -1,
+                },
+                particles: {
+                    color: {
+                        value: "#999999",
+                    },
+                    links: {
+                        color: "#999999",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.5,
+                        width: 1,
+                    },
+                    collisions: {
+                        enable: false,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 3,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 800,
+                        },
+                        value: 80,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 1, max: 5 },
+                    },
+                },
+                detectRetina: true,
+            }}
+        />
+    )
+}
 
 export default function Home() {
     const auth = useAuth()
 
     return (
         <div>
+            <Animation />
             <Container>
                 <div className="welcome-bar">
                     <div className="text-center my-5">
